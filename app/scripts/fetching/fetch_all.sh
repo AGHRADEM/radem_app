e81e6286250fd3b1c7e23bca0ef764e6f1b0f3ae
Szymon Bednorz <dsonyyy@gmail.com>
2024-11-25 14:22:47 +0100

    deploy: cyfronet config

--- app/scripts/fetching/fetch_all.sh
+++ app/scripts/fetching/fetch_all.sh
@@ -7,9 +7,9 @@ SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
 TIMEOUT=30m
 
 # Data fetching
-echo "============= FETCHING RADEM DATA =============" 
-timeout ${TIMEOUT} ${SCRIPTS_DIR}/fetch_radem.sh
-echo -e "\e[32mRADEM DATA FETCHED SUCCESSFULLY\e[0m"
+# echo "============= FETCHING RADEM DATA =============" 
+# timeout ${TIMEOUT} ${SCRIPTS_DIR}/fetch_radem.sh
+# echo -e "\e[32mRADEM DATA FETCHED SUCCESSFULLY\e[0m"
 
 echo "============= FETCHING IREM DATA =============" 
 timeout ${TIMEOUT} ${SCRIPTS_DIR}/fetch_irem.sh
